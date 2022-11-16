f4952596125388231f42ee5f118c5c30f553fcc8
seirios86 <80634237+seirios86@users.noreply.github.com>
2022-11-16 17:02:37 +0900

    Update app-env.sh
    double quotation to single quotation

--- app-env.sh
+++ app-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PROFILE="dev"
-export MYSQL_HOST="172.16.227.224"
-export DB_USERNAME="library"
-export DB_PASSWORD="kic1357!"
+export PROFILE='dev'
+export MYSQL_HOST='172.16.227.224'
+export DB_USERNAME='library'
+export DB_PASSWORD='kic1357!'
